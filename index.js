9f0e2356a06d20e5e1f2d57255d8e8256078e430
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-10 16:50:14 +0100

    Add new info

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hello');
+import color from 'randomcolor';
+import chalk from 'chalk';
+
+const userHue = process.argv[2];
+const userLum = process.argv[3];
+
+const ranColor = color.randomColor({
+    luminosity: userLum,
+    hue: userHue,
+});
+
+function poundSignDesign(str) {
+    console.log(chalk.hex(ranColor).bold(str));
+}
+
+poundSignDesign('###############################');
+poundSignDesign('###############################');
+poundSignDesign('###############################');
+poundSignDesign('#####                     #####');
+poundSignDesign(`#####      ${ranColor}        #####`);
+poundSignDesign('#####                     #####');
+poundSignDesign('###############################');
+poundSignDesign('###############################');
+poundSignDesign('###############################');
+
+//console.log(ranColor);
